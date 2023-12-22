abf996010dda8ed8ccd494cb775b970cefe66614
Anders Rune Jensen <arj03@protonmail.ch>
2023-12-22 12:44:36 +0100

    Split out sender

---
+++ as4_sender.py
@@ -0,0 +1,60 @@
+import requests
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.application import MIMEApplication
+
+# logging
+import http.client as http_client
+import logging
+
+from wsse import encrypt_using_external_xmlsec, encrypt, sign
+from as4 import envelope_as_string, generate_as4_envelope, document_id
+
+def post_multipart(url, filename, keyfile, password, certfile, their_cert):
+    enable_logging()
+
+    document, gzip, doc_id = generate_as4_message_to_post(filename, keyfile, password, certfile, their_cert)
+
+    related = MIMEMultipart('related')
+
+    mt = MIMEText('application', 'soap+xml', 'utf8')
+    mt.set_payload(document)
+    mt.replace_header("Content-Transfer-Encoding", "Binary")
+    mt.add_header("Content-ID", "<root.message@cxf.apache.org>")
+    related.attach(mt)
+
+    mt = MIMEApplication(gzip, "gzip")
+    mt.add_header("Content-Transfer-Encoding", "Binary")
+    mt.add_header("Content-ID", '<' + doc_id[4:] + '>')
+    related.attach(mt)
+
+    # java needs CRLF
+    body = related.__bytes__().replace(b'\n', b'\r\n')
+    headers = dict(related.items())
+
+    r = requests.post(url, data=body, headers=headers)
+    print(r.text)
+
+def generate_as4_message_to_post(filename, keyfile, password, certfile, their_cert):
+    doc_id = document_id()
+    
+    envelope, messaging, body = generate_as4_envelope(filename, doc_id)
+
+    cipher_value, encrypted_gzip, document_hash = encrypt_using_external_xmlsec(filename, their_cert)
+
+    sign(envelope, doc_id, document_hash, body, messaging, keyfile, certfile, password)
+    encrypt(envelope, their_cert, cipher_value, doc_id)
+
+    doc = envelope_as_string(envelope)
+    #print(doc)
+
+    return [doc, encrypted_gzip, doc_id]
+
+def enable_logging():
+    http_client.HTTPConnection.debuglevel = 1
+
+    logging.basicConfig()
+    logging.getLogger().setLevel(logging.DEBUG)
+    requests_log = logging.getLogger("requests.packages.urllib3")
+    requests_log.setLevel(logging.DEBUG)
+    requests_log.propagate = True
--- test.py
+++ test.py
@@ -1,14 +1,4 @@
-from wsse import encrypt_using_external_xmlsec, encrypt, sign
-from as4 import envelope_as_string, generate_as4_envelope, document_id
-
-import requests
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-from email.mime.application import MIMEApplication
-
-# logging
-import http.client as http_client
-import logging
+from as4_sender import post_multipart
 
 # params
 keyfile = "test.key.pem"
@@ -16,61 +6,12 @@ certfile = "cert.pem"
 their_cert = "server-cert.pem"
 password = ''
 filename = 'PEPPOL_TestCase_0232_20231222T1138Z/TestFile_001__BISv3_Invoice.xml'
+url = 'https://phase4-controller.testbed.peppol.org/as4'
+#url = 'https://oxalis.beta.iola.dk/as4'
 
-def generate_as4_message_to_post(filename):
-    doc_id = document_id()
-    
-    envelope, messaging, body = generate_as4_envelope(filename, doc_id)
-
-    cipher_value, encrypted_gzip, document_hash = encrypt_using_external_xmlsec(filename, their_cert)
-
-    sign(envelope, doc_id, document_hash, body, messaging, keyfile, certfile, password)
-    encrypt(envelope, their_cert, cipher_value, doc_id)
-
-    doc = envelope_as_string(envelope)
-    #print(doc)
-
-    return [doc, encrypted_gzip, doc_id]
-
-def enable_logging():
-    http_client.HTTPConnection.debuglevel = 1
-
-    logging.basicConfig()
-    logging.getLogger().setLevel(logging.DEBUG)
-    requests_log = logging.getLogger("requests.packages.urllib3")
-    requests_log.setLevel(logging.DEBUG)
-    requests_log.propagate = True
-
-def post_multipart(url, filename):
-    enable_logging()
-
-    document, gzip, doc_id = generate_as4_message_to_post(filename)
-
-    related = MIMEMultipart('related')
-
-    mt = MIMEText('application', 'soap+xml', 'utf8')
-    mt.set_payload(document)
-    mt.replace_header("Content-Transfer-Encoding", "Binary")
-    mt.add_header("Content-ID", "<root.message@cxf.apache.org>")
-    related.attach(mt)
-
-    mt = MIMEApplication(gzip, "gzip")
-    mt.add_header("Content-Transfer-Encoding", "Binary")
-    mt.add_header("Content-ID", '<' + doc_id[4:] + '>')
-    related.attach(mt)
-
-    # java needs CRLF
-    body = related.__bytes__().replace(b'\n', b'\r\n')
-    headers = dict(related.items())
-
-    r = requests.post(url, data=body, headers=headers)
-    print(r.text)
-    
-# 9922:ngtbcntrlp1001
-# 9922:NGTBCNTRLP1001
-receiver = '9928:CY99990011B' # final URL er buggy
-receiver = '0188:2011001016148' # good example
-receiver = '9922:NGTBCNTRLP1001' # from test certification file
+#receiver = '9928:CY99990011B' # final URL is buggy
+#receiver = '0188:2011001016148' # good example
+#receiver = '9922:NGTBCNTRLP1001' # from test certification file
 
 # why doesn't test cert do this?
 #smp_domain = get_domain_using_sml(receiver)
@@ -80,7 +21,4 @@ receiver = '9922:NGTBCNTRLP1001' # from test certification file
 #smp_contents = get_smp_info(smp_domain, receiver)
 #extract_as4_information(smp_contents)
 
-#generate_as4_message_to_post('TestFile_003__BISv3_Invoice.xml')
-#url = 'https://oxalis.beta.iola.dk/as4'
-url = 'https://phase4-controller.testbed.peppol.org/as4'
-post_multipart(url, filename)
+post_multipart(url, filename, keyfile, password, certfile, their_cert)

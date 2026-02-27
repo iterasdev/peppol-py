9d1737f8a0791f04ed00b60a71b19ee71e3341cb
Raphael Michel <michel@pretix.eu>
2026-02-27 11:34:56 +0100

    Add missing raise_for_status() error handling on AS4 submission (#18)
    * Add missing raise_for_status() error handling on AS4 submission
    
    * Improve error handling for non-XML error responses

--- src/peppol_py/as4_sender.py
+++ src/peppol_py/as4_sender.py
@@ -38,6 +38,18 @@ def post_edelivery_as4_document(endpoint_url, body, headers, timeout):
     try:
         response_xml = etree.fromstring(r.content)
     except (etree.XMLSyntaxError, ValueError) as e:
+        if r.status_code >= 400:
+            raise make_sendpeppol_error(
+                "Endpoint error",
+                'server-error',
+                temporary=True,
+                url=endpoint_url,
+                endpoint={
+                    'status_code': r.status_code,
+                    'error_xml': None,
+                    'error_msg': r.content,
+                }
+            )
         raise make_sendpeppol_error(str(e), 'server-error', temporary=True, url=endpoint_url)
 
     if not r.status_code == 200 or response_xml.find('.//{*}Header//{*}Receipt') is None:
